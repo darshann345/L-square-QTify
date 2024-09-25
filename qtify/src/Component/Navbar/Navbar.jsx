168cf6179b5546ef902e51e983c5b0f281873257
Darshan N <darshann679596@gmail.com>
2024-09-25 09:25:52 +0530

    search bar check test

--- qtify/src/Component/Navbar/Navbar.jsx
+++ qtify/src/Component/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import Logo from "../Logo/Logo";
-import SearchBar from "../Search/Search";
+import SearchBar from "../Search/Search"
 import Button from "../Buttons/Button";
 
 const Navbar = () => {
@@ -12,7 +12,6 @@ const Navbar = () => {
                 <Logo />
                 <SearchBar search={"Search a song of your choice"} />
                 <Button children={"Give Feedback"} />
-                <h1>Dummy</h1>
             </nav>
 
         </>
